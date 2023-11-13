724a71478813fba5312012560cc867f4cebb8773
nhndev <ngohuunhan2001pro@gmail.com>
2023-11-13 21:55:19 +0700

    Implement Redis

--- index.js
+++ index.js
@@ -1,122 +1,157 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-app.use(express.json())
+app.use(express.json());
 
-const connectDB = require('./connectMongo')
+const connectDB = require("./connectMongo");
 
-connectDB()
+connectDB();
 
-const BookModel = require('./models/book.model')
+const BookModel = require("./models/book.model");
+const redis = require('./redis')
 
-app.get('/api/v1/books', async (req, res) => {
+const deleteKeys = async (pattern) => {
+  const keys = await redis.keys(`${pattern}::*`)
+  console.log(keys)
+  if (keys.length > 0) {
+    redis.del(keys)
+  }
+}
 
-    const { limit = 5, orderBy = 'name', sortBy = 'asc', keyword } = req.query
-    let page = +req.query?.page
+app.get("/api/v1/books", async (req, res) => {
+  const { limit = 5, orderBy = "name", sortBy = "asc", keyword } = req.query;
+  let page = +req.query?.page;
 
-    if (!page || page <= 0) page = 1
+  if (!page || page <= 0) page = 1;
 
-    const skip = (page - 1) * +limit
+  const skip = (page - 1) * + limit;
 
-    const query = {}
+  const query = {};
 
-    if (keyword) query.name = { "$regex": keyword, "$options": "i" }
+  if (keyword) query.name = { $regex: keyword, $options: "i" };
 
-    try {
-        const data = await BookModel.find(query).skip(skip).limit(limit).sort({[orderBy]: sortBy})
-        const totalItems = await BookModel.countDocuments(query)
-        return res.status(200).json({
-            msg: 'Ok',
-            data,
-            totalItems,
-            totalPages: Math.ceil(totalItems / limit),
-            limit: +limit,
-            currentPage: page
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
+  const key = `Book::${JSON.stringify({query, page, limit, orderBy, sortBy})}`
+  let response = null
+  try {
+    const cache = await redis.get(key)
+    if (cache) {
+      response = JSON.parse(cache)
+    } else {
+      const data = await BookModel.find(query)
+      .skip(skip)
+      .limit(limit)
+      .sort({ [orderBy]: sortBy });
+      const totalItems = await BookModel.countDocuments(query);
+
+      response = {
+        msg: "Ok",
+        data,
+        totalItems,
+        totalPages: Math.ceil(totalItems / limit),
+        limit: +limit,
+        currentPage: page,
+      }
+
+      redis.setex(key, 600, JSON.stringify(response))
     }
-})
+    
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
-app.get('/api/v1/books/:id', async (req, res) => {
-    try {
-        const data = await BookModel.findById(req.params.id)
+app.get("/api/v1/books/:id", async (req, res) => {
+  try {
+    const data = await BookModel.findById(req.params.id);
 
-        if (data) {
-            return res.status(200).json({
-                msg: 'Ok',
-                data
-            })
-        }
-
-        return res.status(404).json({
-            msg: 'Not Found',
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
+    if (data) {
+      return res.status(200).json({
+        msg: "Ok",
+        data,
+      });
     }
-})
 
-app.post('/api/v1/books', async (req, res) => {
-    try {
-        const { name, author, price, description } = req.body
-        const book = new BookModel({
-            name, author, price, description
-        })
-        const data = await book.save()
-        return res.status(200).json({
-            msg: 'Ok',
-            data
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
+    return res.status(404).json({
+      msg: "Not Found",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
-app.put('/api/v1/books/:id', async (req, res) => {
-    try {
-        const { name, author, price, description } = req.body
-        const { id } = req.params
+app.post("/api/v1/books", async (req, res) => {
+  try {
+    const { name, author, price, description } = req.body;
+    const book = new BookModel({
+      name,
+      author,
+      price,
+      description,
+    });
+    const data = await book.save();
+    deleteKeys('Book')
+    return res.status(200).json({
+      msg: "Ok",
+      data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
-        const data = await BookModel.findByIdAndUpdate(id, {
-            name, author, price, description
-        }, { new: true })
+app.put("/api/v1/books/:id", async (req, res) => {
+  try {
+    const { name, author, price, description } = req.body;
+    const { id } = req.params;
 
-        return res.status(200).json({
-            msg: 'Ok',
-            data
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
+    const data = await BookModel.findByIdAndUpdate(
+      id,
+      {
+        name,
+        author,
+        price,
+        description,
+      },
+      { new: true }
+    );
+    deleteKeys('Book')
+    return res.status(200).json({
+      msg: "Ok",
+      data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
-app.delete('/api/v1/books/:id', async (req, res) => {
-    try {
-        await BookModel.findByIdAndDelete(req.params.id)
-        return res.status(200).json({
-            msg: 'Ok',
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
+app.delete("/api/v1/books/:id", async (req, res) => {
+  try {
+    await BookModel.findByIdAndDelete(req.params.id);
+    deleteKeys('Book')
+    return res.status(200).json({
+      msg: "Ok",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT)
-})
+  console.log("Server is running on port " + PORT);
+});
